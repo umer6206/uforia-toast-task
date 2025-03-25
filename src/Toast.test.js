25ee1b8e7e43df2038a30d41ae18cb98c4d2e52e
Muhammad Umer <umerm6206gmail.com>
2025-03-25 09:16:37 +0500

    import issue resolved

--- src/Toast.test.js
+++ src/Toast.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent, act } from "@testing-library/react";
-import Toast from "./Toast";
+import Toast from "./components/Toast";
 
 describe("Toast Component", () => {
   const mockFormSubmission = {
